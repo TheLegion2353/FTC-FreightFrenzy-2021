f8d8be7a6ace1d0b4bee4db79014b67764e4f287
Geeoon Chung <geeoon13@gmail.com>
2021-10-19 15:58:51 -0500

    Imported libraries

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MainTeleOp.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+import org.firstinspires.ftc.teamcode.Robot.HardwareController;
+import org.firstinspires.ftc.teamcode.Robot.*;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Hardware;
+
+import org.firstinspires.ftc.teamcode.Robot.Robot;
+
+@TeleOp(name="Main Mecanum", group="Driver Controlled")
+public class MainTeleOp extends OpMode {
+	private Drivetrain drive = null;
+	@Override
+	public void init() {
+		drive = new Drivetrain(Drivetrain.ControlType.ARCADE, gamepad1, telemetry);
+		drive.setTopRight(DcMotorEx.RunMode.RUN_USING_ENCODER, hardwareMap.get(DcMotorEx.class, "topRightDrive"));
+		drive.setTopLeft(DcMotorEx.RunMode.RUN_USING_ENCODER, hardwareMap.get(DcMotorEx.class, "topLeftDrive"));
+		drive.setBottomRight(DcMotorEx.RunMode.RUN_USING_ENCODER, hardwareMap.get(DcMotorEx.class, "backRightDrive"));
+		drive.setBottomLeft(DcMotorEx.RunMode.RUN_USING_ENCODER, hardwareMap.get(DcMotorEx.class, "backLeftDrive"));
+	}
+
+	@Override
+	public void loop() {
+		drive.update();
+	}
+}
